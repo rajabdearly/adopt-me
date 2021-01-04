7afa27cf94f16d07e9962dc1c79148e9ef631d2b
Rajab Azizli <rajabdearly@gmail.com>
2021-01-05 00:07:13 +0200

    npm init, configuring prettier

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const Pet = ({name, animal, breed}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
@@ -7,17 +7,16 @@ const Pet = ({name, animal, breed}) => {
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1",{}, "Adopt me!"),
-      React.createElement(Pet, { name: "Toppush", animal: "Dog", breed: "Lazy"}),
-      React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel"}),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed"})
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, { name: "Toppush", animal: "Dog", breed: "Lazy" }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App),
-document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
